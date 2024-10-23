09f842c7d374ee9461a2a80d39042d1f30928aa6
Omurden Cengiz <omurden@Omurdens-MacBook-Pro.local>
2024-10-23 14:25:18 +0200

    dummy push

--- src/test/java/com/rijks/RijksmuseumAPITests.java
+++ src/test/java/com/rijks/RijksmuseumAPITests.java
@@ -56,5 +56,5 @@ public class RijksmuseumAPITests {
             .body("artObject.title", notNullValue())
             .body("artObject.objectNumber", equalTo(objectNumber));
     }
-    //dummy text24
+    //dummy text2456
 }
